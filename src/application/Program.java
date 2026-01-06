2575af62a8f1088eb1c6068c9e172c7a316458b6
Apollonic <apollo.nicolas00@gmail.com>
2026-01-06 20:35:15 -0300

    classe department

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+    static void main() {
+
+        Department obg = new Department(1,"Books");
+        System.out.println(obg);
+
+    }
+
+}
