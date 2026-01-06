3d421365e2273069019d9ab3ad9e26eb06273537
Apollonic <apollo.nicolas00@gmail.com>
2026-01-06 20:50:08 -0300

    classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,18 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     static void main() {
 
         Department obg = new Department(1,"Books");
         System.out.println(obg);
+
+    Seller seller = new Seller(1,"Bob","sla@gmail.com",new Date(),900.0,obg);
+        System.out.println(seller);
 
     }
 
